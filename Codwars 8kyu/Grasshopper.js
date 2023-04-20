150c6fc7c97e97beb57d3403c6ea6c35a00f39e7
Antony Shaga <antonyshaga@gmail.com>
2023-04-20 13:35:02 +0300

    first commit

---
+++ Codwars 8kyu/Grasshopper.js
@@ -0,0 +1,11 @@
+// My solution
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+
+//Best Practices
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
