17cf4b201c1e12df6a03446cc75d7cbef6d83b78
Antony Shaga <antonyshaga@gmail.com>
2023-04-21 18:33:02 +0300

    first commit

---
+++ Codwars 8kyu/ReversedWords.js
@@ -0,0 +1,10 @@
+// My solution
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
+
+//Best Practices
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
