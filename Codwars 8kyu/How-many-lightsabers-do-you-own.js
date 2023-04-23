979147152a51472ba4b66f320717f08956233b85
Antony Shaga <antonyshaga@gmail.com>
2023-04-23 17:49:20 +0300

    first commit

---
+++ Codwars 8kyu/How-many-lightsabers-do-you-own.js
@@ -0,0 +1,11 @@
+// My solution
+function howManyLightsabersDoYouOwn(name) {
+  return name === 'Zach' ? 18 : 0
+}
+
+
+//Best Practices
+
+function howManyLightsabersDoYouOwn(name) {
+  return name === 'Zach' ? 18 : 0;
+}
