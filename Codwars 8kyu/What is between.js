13786d2b26d155c86e185d0b53dd7bee07d18a38
Anthony Shaga <antonyshaga@gmail.com>
2023-06-11 11:08:44 +0300

    first commit

---
+++ Codwars 8kyu/What is between.js
@@ -0,0 +1,20 @@
+// My solution
+function between(a, b) {
+  // your code here
+  let arr = [] 
+  for (let i = a; i <= b; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+//Best Practices
+
+function between(a, b) {
+  // your code here
+  arr = []
+  for(i = a;i <= b; i++){ 
+      arr.push(i)
+  }
+  return arr
+}
